4bc95fb41414da6e2a2097a77be24d624772cce3
Pascal Thelke <p.thelke@googlemail.com>
2024-03-15 18:03:07 +0100

    genereted some components to get used to it

---
+++ src/app/main-content/landing-page/landingPage.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { NavbarComponent } from './navbar/navbar.component'
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule, NavbarComponent],
+    template: `
+    <section>
+        <div><app-navbar></app-navbar></div>
+        <h1 class="fontRaleway">SAKURA RAMEN</h1>
+        <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
+    </section>
+    `,
+    styleUrls: ['./landingPage.components.scss']
+
+})
+export class LandingPageComponent { }
